1f40f16b8a8b5ddafd96a4e3546d83a7a662761a
Lappg <lappg1@runsystem.net>
2024-04-30 10:37:40 +0700

    setup server

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import * as serveStatic from 'serve-static'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', allowedHeaders: 'Content-Type, Accept' })
   app.setGlobalPrefix('api/v1')
   app.useGlobalPipes(new ValidationPipe(validationOptions))
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
